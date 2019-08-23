30fad12bc132d2851ddd8d516070e4ed4ce5fc53
Fill <gcodepost@gmail.com>
2019-08-23 07:01:44 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () =>{
+    return (
+        <div className="content">
+            <div>
+                <img src="https://of-crimea.ru/plug/Peschanye-plyazhi-Kryma.jpg" alt=""/>
+            </div>
+            <div>
+                a+d
+            </div>
+            <div>
+                my post
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
