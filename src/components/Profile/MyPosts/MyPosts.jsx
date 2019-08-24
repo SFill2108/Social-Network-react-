e1107f0c364c12299f155abf882e84ae173f5423
Fill <gcodepost@gmail.com>
2019-08-24 07:18:14 +0300

    add 1st props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
+import Post from './Post/Post'
 const MyPosts = () =>{
     return (
         <div>
@@ -8,12 +9,8 @@ const MyPosts = () =>{
                 New posts
             </div>
             <div className={classes.posts}>
-                <div className={classes.item}>
-                    post1
-                </div>
-                <div className={classes.item}>
-                    post2
-                </div>
+                <Post message="Hello, world!" />
+                <Post message="First post, LOL"/>
             </div>
         </div>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Profile.module.css';
-import MyPosts from './MyPosts/Posts/MyPosts'
+import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () =>{
     return (
