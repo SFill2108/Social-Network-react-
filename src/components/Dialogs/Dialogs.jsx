dee6f3f6ae85cbd4b238bfcdac041ac5aba94b0c
Fill <gcodepost@gmail.com>
2019-08-26 09:08:49 +0300

    dialogs layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import classes from './Dialogs.modules.css'
+import classes from './Dialogs.module.css'
 const Dialogs = (props) =>{
     return(
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog}>
+                    Маша
+                </div>
+                <div className={classes.dialog + " " + classes.active}>
+                    Даниил
+                </div>
+                <div className={classes.dialog}>
+                    Денис
+                </div>
+                <div className={classes.dialog}>
+                    Владислав
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Прив</div>
+                <div className={classes.message}>Как дела</div>
+                <div className={classes.message}>приходи</div>
+            </div>
         </div>
     );
 }
